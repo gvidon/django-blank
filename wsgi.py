9c03db5dadc0c2c5596517b9f8126998c063f3c6
nide <nenegoro@gmail.com>
2012-05-08 02:00:33 +0400

    configurable wsgi script

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,9 @@
 import os, sys
 from gevent import monkey, wsgi
 
+WSGI_HOST = '127.0.0.1'
+WSGI_PORT = 8888
+
 monkey.patch_socket()
 
 sys.path.insert(0, os.path.dirname(__file__))
@@ -9,4 +12,4 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 from django.core.handlers.wsgi import WSGIHandler
 
-wsgi.WSGIServer(('127.0.0.1', 8017), WSGIHandler()).serve_forever()
+wsgi.WSGIServer((WSGI_HOST, WSGI_PORT), WSGIHandler()).serve_forever()
