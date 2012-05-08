8c8e8311f6e4de14dd4c1ada4ec5ed3e2f52b268
nide <nenegoro@gmail.com>
2012-05-08 16:51:49 +0400

    Serving static files in DEBUG mode

--- urls.py
+++ urls.py
@@ -5,13 +5,12 @@ from django.conf.urls.defaults import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'blank.views.home', name='home'),
-    # url(r'^blank/', include('blank.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+	urlpatterns += staticfiles_urlpatterns()
